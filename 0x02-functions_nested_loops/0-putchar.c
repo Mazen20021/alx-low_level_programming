6995e3b410eacded66d59b698fca626e1515edab
Mazen <you@example.com>
2023-06-21 09:01:51 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,10 @@
 	/**
 	 * main - print _putchar
 	 * is written by mbah
-	 * Return: 0
+	 * Return: 0 (Success)
 	 */
-
-
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +16,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
-
+}
